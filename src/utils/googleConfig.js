d0cbf0487206bb3d13827d528c705b8204396ff7
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-11-22 21:01:50 +0530

    now it is correct

--- src/utils/googleConfig.js
+++ src/utils/googleConfig.js
@@ -1,6 +1,6 @@
 import {google} from 'googleapis'
 
 export const oauth2client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID,process.env.GOOGLE_CLIENT_SECRET,
-    "https://quiz-app-s80k.onrender.com/api/v1"
+    "https://quiz-app-s80k.onrender.com/api/v1/auth/google/callback"
 
 )
