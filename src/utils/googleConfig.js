5bfad4726c0857a32e8ba7f61f7bb413bc9b6138
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-11-22 20:52:58 +0530

    re-entered callbackurl

--- src/utils/googleConfig.js
+++ src/utils/googleConfig.js
@@ -1,3 +1,6 @@
 import {google} from 'googleapis'
 
-export const oauth2client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID,process.env.GOOGLE_CLIENT_SECRET,[process.env.ORIGIN])
+export const oauth2client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID,process.env.GOOGLE_CLIENT_SECRET,
+    "https://quiz-e-knowledge.netlify.app/auth/google/callback"
+
+)
