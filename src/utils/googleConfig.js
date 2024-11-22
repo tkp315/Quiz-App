b883922e42fd623f497e0aafb5156bde007b0348
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-11-22 21:09:48 +0530

    added a new uri

--- src/utils/googleConfig.js
+++ src/utils/googleConfig.js
@@ -1,6 +1,3 @@
 import {google} from 'googleapis'
 
-export const oauth2client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID,process.env.GOOGLE_CLIENT_SECRET,
-    "https://quiz-app-s80k.onrender.com/api/v1/auth/google/callback"
-
-)
+export const oauth2client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID,process.env.GOOGLE_CLIENT_SECRET,'postmessage')
